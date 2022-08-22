9d52edf28d75f377feb9cba9f6c47f2ddb930548
Shervin Amini <shervin.amini@hotmail.fr>
2022-08-22 16:31:39 +0100

    ok except CSS & opt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  get "ask", to: "questions#ask"
+  get "answer", to: 'questions#answer'
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
